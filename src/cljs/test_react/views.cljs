69ea029473ab8919f706e3f1d71d8ec44e4d4099
Andrew Lai <andrew.s.lai5@gmail.com>
2019-07-28 14:06:52 -0500

    Get object moving with animation library.
    Add start-stop behavior on click
    Remove some old code

--- src/cljs/test_react/views.cljs
+++ src/cljs/test_react/views.cljs
@@ -5,8 +5,10 @@
    [stylefy.core :as stylefy :refer [use-style]]
    [garden.units :as g]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Setup/init
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (stylefy/init)
-
 (def icon-list ["images/accessibility.svg"
                 "images/favorite.svg"
                 "images/find-in-page.svg"
@@ -16,69 +18,114 @@
                 "images/language.svg"
                 "images/lock.svg"])
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Animations
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn frac->rad [x]
+  (* 2 Math/PI x))
+
+(def n-icons (count icon-list))
+
+(defn create-keyframe [i]
+  (stylefy/keyframes (str "expand-item-" i)
+                     [(g/percent 50)
+                      {:background-color "red"
+                       :top (str (-> i
+                                     (/ 8)
+                                     frac->rad
+                                     Math/sin
+                                     (* 50)) "px")
+                       :left "-100px"}]
+                     [(g/percent 100)
+                      {:background-color "black"
+                       :top (str (-> i
+                                     (/ 8)
+                                     frac->rad
+                                     Math/sin
+                                     (* 50)) "px")
+                       :left "200px"}]))
+
 (defn myfunction []
   (let [mybutton (-> js/document
                      (.getElementById "mybutton"))]
     (println mybutton)
-    (set! (.. mybutton -style -top) "10px")
+    (set! (.. mybutton -style -top) "100px")
     (println (.. mybutton -style -top))))
 
-(defn main-circle-icon []
-  [:div.main-image {:style {:position "relative"
-                            :top "50%"
-                            :left "50%"}}
-   [:button#mybutton  {:onClick myfunction
-                       :style {:position "absolute"
-                               :background-image "url(images/home.svg)"
-                               :background-repeat "no-repeat"
-                               :background-position-x "center"
-                               :background-position-y "center"
-                               :background-color "blue"
-                               :height "80px"
-                               :width "80px"
-                               :border-radius "80px"
-                               :animation-delay "0.24s"
-                               :animation-duration "0.4s"
-                               :animation-timing-function "ease-out"
-                               :animation-name "contract-item"
-                               :animation-fill-mode "backwards"
-                               }}]])
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; DOM
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-(defn outer-circle-icons [img-path]
-  [:div.outer-image {:style {:position "relative"
-                             :top "50%"
-                             :left "50%"}}
-   [:img {:src img-path
-          :style {:position "absolute"
-                  :background-color "blue"
-                  :height "80px"
-                  :width "80px"
-                  :border-radius "80px"}}]])
 
-#_(stylefy/keyframes "simple-animation"
-                     [:from
-                      {:background-color "red"}]
-                     [:to
-                      {:background-color "blue"}])
-
-(stylefy/keyframes "expand-item"
-                   [(g/percent 50)
-                    {:background-color "red"}]
-                   [(g/percent 100)
-                    {:background-color "blue"}])
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; TEST/EXAMPLE CODE
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def simple-box {:border "1px solid black"
                  :background-color "#FFDDDD"
                  :text-align :center
                  :padding "5px"
                  :width "150px"
-                 :height "150px"})
+                 :height "150px"
+                 :position "absolute"
+                 :background-repeat "no-repeat"
+                 :background-position-x "center"
+                 :background-position-y "center"
+                 :border-radius "80px"})
 
-(def animated-box (merge simple-box
-                         {:animation-name "expand-item"
-                          :animation-duration "3s"
-                          :animation-iteration-count "infinite"}))
+
+(stylefy/keyframes "flash-item-2"
+                   [(g/percent 0)
+                    {:background-color "green"
+                     :left "0px"}]
+                   [(g/percent 50)
+                    {:background-color "yellow"
+                     :left "200px"}]
+                   [(g/percent 100)
+                    {:background-color "green"
+                     :left "0px"}])
+
+(def animated-box-2 (merge simple-box
+                           {:background-image "url(images/home.svg)"
+                            :border-radius "80px"
+                            :animation-name "flash-item-2"
+                            :animation-duration "3s"
+                            :animation-iteration-count "infinite"}))
+
+(defn make-box-style [img] (merge simple-box
+                                  {:background-image (str "url(" img ")" )
+                                   :border-radius "80px"
+                                   :animation-name "flash-item-2"
+                                   :animation-duration "3s"
+                                   :animation-delay (str (/ 33 (rand-int 100)) "s")
+                                   :animation-iteration-count "infinite"}))
+(make-box-style (first icon-list))
+
+(defonce active? (atom true))
+(defn toggle-keyframe []
+  (if @active?
+    (stylefy/keyframes "flash-item-2")
+    (stylefy/keyframes "flash-item-2"
+                       [(g/percent 0)
+                        {:background-color "green"
+                         :left "0px"}]
+                       [(g/percent 50)
+                        {:background-color "yellow"
+                         :left "200px"}]
+                       [(g/percent 100)
+                        {:background-color "green"
+                         :left "0px"}]))
+  (reset! active? (not @active?)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; FINAL RENDERING
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn make-button [img]
+  [:button (merge {:onClick toggle-keyframe}
+                  (use-style (make-box-style img)))])
+
+(defn make-buttons []
+  [:div (map make-button icon-list)])
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
@@ -88,11 +135,17 @@
                                     :top "100px"
                                     :left "10%"
                                     :width "80%"
-                                    :height "80%"}}
-      (map outer-circle-icons icon-list)
-      [main-circle-icon]]
-     [:div#animated-box (use-style animated-box)]
+                                    :height "80%"}}]
+
+     [:div.main-image {:style {:position "relative"
+                               :top "50%"
+                               :left "50%"}}
+      [:button#animated-box-2 (merge {:onClick toggle-keyframe}
+                                     (use-style animated-box-2))]]
+     (make-buttons)
+     [:div#s]
      ]))
+
 
 
 (comment
